7b8f954a325aba425514f9648942bc540ab1ded1
ramadhani-22 <aramadhani2212@gmail.com>
2023-02-10 11:13:17 +0700

    remove debug

--- restoran.java
+++ restoran.java
@@ -1,7 +1,7 @@
 public class restoran {
 
 	public static void main(String[] args) {
-		System.out.println("Menyiapkan data ...");
+		System.out.println("Menyiapkan data...");
 
 		// migration
 		pelanggan.buatTabel();
